0da3e6085fe97fa944c63b60fa95a534c41caef3
wills <godswillijeze@gmail.com>
2024-04-23 11:53:08 +0100

    routing issues 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { landingRoutes } from "./routes/landing-routes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { landingRoutes } from "./routes/landing-routes";
+import LandingLayout from "./layout/landing";
+import Homepage from "./pages/landing/homepage";
 
-const router = createBrowserRouter([...landingRoutes]);
+// const router = createBrowserRouter([...landingRoutes]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  // return <RouterProvider router={router} />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<LandingLayout />}>
+            <Route path="/" element={<Homepage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
