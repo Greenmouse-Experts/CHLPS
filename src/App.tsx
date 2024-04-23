90492cb39ab8f00700a7b67c52cc83573d467925
wills <godswillijeze@gmail.com>
2024-04-23 12:10:08 +0100

    checking routes 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { landingRoutes } from "./routes/landing-routes";
+import { BrowserRouter,  Route,  Routes } from "react-router-dom";
+// import { landingRoutes } from "./routes/landing-routes";
 import { ChakraProvider } from "@chakra-ui/react";
+import Homepage from "./pages/landing/homepage";
 
-const router = createBrowserRouter([...landingRoutes]);
+// const router = createBrowserRouter([...landingRoutes]);
 
 function App() {
   return (
+    <BrowserRouter>
     <ChakraProvider>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+      </Routes>
     </ChakraProvider>
+    </BrowserRouter>
   );
 }
 
